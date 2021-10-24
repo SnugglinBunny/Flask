e891dfc3127eedfa7328cf7bb1acba6849887b14
John <defiantnarwhal@gmail.com>
2021-10-25 00:34:43 +0100

    Added login/logout and accounts/session

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SECRET_KEY"] = "b8947dde6d9375914e8e81dfd2781a5b"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 from blog import routes
